f56bb91d5bf96600c8ba7f95c2649e86b115fcd3
dustin-H <dustin.hoffner@gmail.com>
2014-01-22 11:49:53 +0100

    Version update: Nothing changes

--- bin/server/serverBuild.js
+++ bin/server/serverBuild.js
@@ -1,5 +1,5 @@
-//Server-Build Version: BETA => 0.634
-console.log(""); console.log("pragm-Websocket-Server => BUILD 0.634 BETA"); console.log("");
+//Server-Build Version: BETA => 0.635
+console.log(""); console.log("pragm-Websocket-Server => BUILD 0.635 BETA"); console.log("");
     /******************************************************************************************
 #
 #       Copyright 2014 Dustin Robert Hoffner
