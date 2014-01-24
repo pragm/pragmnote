37bcb8f1af256c2036335448ea5849cc333a1eb9
dustin-H <dustin.hoffner@gmail.com>
2014-01-24 18:00:26 +0100

    Solution for Issue #51
    [Client] UI Login CSS background-position:100px 50%;

--- bin/server/serverBuild.js
+++ bin/server/serverBuild.js
@@ -1,5 +1,5 @@
-//Server-Build Version: BETA => 0.2.674
-console.log(""); console.log("pragm-Websocket-Server => BUILD 0.2.674 BETA"); console.log("");
+//Server-Build Version: BETA => 0.2.675
+console.log(""); console.log("pragm-Websocket-Server => BUILD 0.2.675 BETA"); console.log("");
     /******************************************************************************************
 #
 #       Copyright 2014 Dustin Robert Hoffner
