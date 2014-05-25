c404d83d928fd2b155f5c22a51bee2d3099510c6
dustin-H <dustin.hoffner@gmail.com>
2014-05-25 20:57:03 +0200

    new config file for without php use

---
+++ bin/client/config.js
@@ -0,0 +1,9 @@
+
+var globalconfig = {
+    "serveraddress": "http://localhost:8080/",
+    "addressalert": false,
+    "list": [{"domain": "demo.pragm.de", "protocol": "http", "port": 8080, "prio": 5}],
+    "defaultport": 8080,
+    "otherPorts": [9343,9342,80]
+}
+
